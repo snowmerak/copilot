498b2e10daae5870c25f0f31eda728fdb5b12f28
snowmerak <snowmerak@outlook.com>
2024-12-12 00:14:03 +0900

    make base prompt

---
+++ prompt/base.go
@@ -0,0 +1,21 @@
+package prompt
+
+import "strings"
+
+const Base = `Generate a code snippet from template and data.
+The template is a single file, package or class that is used to generate code.
+The data is a pseudo code or a inspiration that is used to generate code.
+You only need to provide the template and data, the code will be generated automatically.
+You only reply to the prompt with the generated code.
+Do not explain the code, just provide the code.`
+
+func Make(template string, query string) string {
+	builder := strings.Builder{}
+	builder.WriteString("# System\n\n")
+	builder.WriteString(Base)
+	builder.WriteString("\n\n# Template\n\n```\n")
+	builder.WriteString(template)
+	builder.WriteString("\n```\n\n# Data\n\n")
+	builder.WriteString(query)
+	return builder.String()
+}
