d3bb62c9a34138581eedde7072e23d7da93a0bdf
snowmerak <snowmerak@outlook.com>
2024-12-12 00:14:13 +0900

    make main func

--- main.go
+++ main.go
@@ -1,1 +1,83 @@
-package copilot
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/AlecAivazis/survey/v2"
+
+	"github.com/snowmerak/copilot/prompt"
+	"github.com/snowmerak/copilot/qwen"
+	"github.com/snowmerak/copilot/template"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cli, err := qwen.New(qwen.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	filePath := ""
+	if err := survey.AskOne(&survey.Input{
+		Message: "Input Output File path",
+	}, &filePath); err != nil {
+		panic(err)
+	}
+
+	if err := os.MkdirAll(filepath.Base(filePath), os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	temple := ""
+	if err := survey.AskOne(&survey.Select{
+		Message: "Select Template",
+		Options: template.List(),
+	}, &temple); err != nil {
+		panic(err)
+	}
+
+	query := ""
+	if err := survey.AskOne(&survey.Multiline{
+		Message: "Input Request",
+	}, &query); err != nil {
+		panic(err)
+	}
+
+	pmt := prompt.Make(template.Get(temple), query)
+
+	log.Printf("Start generating code from template %s", temple)
+	resp, err := cli.Generate(ctx, pmt)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Code generated successfully")
+
+	if _, err := writer.WriteString(resp); err != nil {
+		panic(err)
+	}
+
+	if err := writer.Flush(); err != nil {
+		panic(err)
+	}
+
+	if err := file.Close(); err != nil {
+		panic(err)
+	}
+
+	log.Printf("File %s created", filePath)
+}
